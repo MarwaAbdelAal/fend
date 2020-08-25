157812383cdc8541ffd0e1998d7b72abd854b799
marwaabdelaal <marwaabdelaal112@gmail.com>
2020-08-25 23:57:30 +0200

    landing page

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -17,7 +17,11 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.querySelectorAll('section');
+let navLinks;
+let listItems;
+const mybutton = document.getElementById('myBtn');
+const nav = document.querySelector('nav');
 
 /**
  * End Global Variables
@@ -25,7 +29,42 @@
  * 
 */
 
+function buildNav() {
+    const uList = document.querySelector('ul');
+    for (let i = 0; i < 4; i++) {
+        const listItem = document.createElement('li');
+        const itemLink = document.createElement('a');
+        itemLink.setAttribute('class', 'menu__link');
+        itemLink.href = `#section${i+1}`;
+        itemLink.innerHTML = `Section${i+1}`;
+        listItem.appendChild(itemLink);
+        uList.appendChild(listItem);
+    }
+    nav.style.display = 'none';
+}
 
+function makeActive(section, index) {
+        const { top } = section.getBoundingClientRect();
+        if (top <= 100 && top >= -100){
+            navActive(listItems[index], index);  // Activating the navigation item when a section is in the viewport
+            section.classList.add('your-active-class');
+            for (let i = 0; i < sections.length; i++) {
+                if (index !== i) sections[i].classList.remove('your-active-class');
+            }
+    }
+}
+
+// function to activate the navigation item when a section is in the viewport
+function navActive(listItem, index) {
+    listItem.setAttribute('style', 'background: #666; color: #ffff;')
+    for (let i = 0; i < listItems.length; i++) {
+        if (i !== index) listItems[i].setAttribute('style',  'background: #fff; color: #000;')
+    }
+}
+
+function getSectionByHref(aTag) {
+    return document.getElementById(aTag.href.split('#')[1]);
+}
 
 /**
  * End Helper Functions
@@ -34,13 +73,44 @@
 */
 
 // build the nav
+buildNav();
+navLinks = document.querySelectorAll('a');
+listItems = document.querySelectorAll('li');
 
 
 // Add class 'active' to section when near top of viewport
+function activeListener(event) {
+    for (let i = 0; i < sections.length; i++) {
+        makeActive(sections[i], i);
+    }
+    // Hide fixed navigation bar while not scrolling
+    nav.style.display = 'block';
+    setTimeout(() => {
+        // nav stays visible on top of the page 
+        if (window.pageYOffset > 150) nav.style.display = 'none';
+    }, 2000);
+}
 
+// Add a scroll to top button on the page
+function addScrollBtn(event) {
+    if (window.pageYOffset > 100) {
+        mybutton.style.display = 'block';
+    }
+    else mybutton.style.display = 'none';
+}
+
+// scroll up button clicked
+function scrollToTop(event) {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function scrollListener(event) {
+    event.preventDefault();
+    const a = event.target;
+    const section = getSectionByHref(a);
+    section.scrollIntoView({ behavior: 'smooth' });
+}
 
 /**
  * End Main Functions
@@ -51,7 +121,21 @@
 // Build menu 
 
 // Scroll to section on link click
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', scrollListener);
+}
 
 // Set sections as active
 
+document.addEventListener('scroll', activeListener);
+document.addEventListener('scroll', addScrollBtn);
 
+// scroll up button clicked
+document.getElementById('myBtn').addEventListener('click', scrollToTop);
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    setTimeout(() => {
+        window.scrollTo({top: 0});
+    }, 100);
+});
