79a8462187f90f7a0299804ae604737eeef1a381
marwaabdelaal <marwaabdelaal112@gmail.com>
2020-09-15 08:25:17 +0200

    updated weather app

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,19 +1,50 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+const server = app.listen(port, runServer);
+
+function runServer() {
+    console.log('server is running...');
+    console.log(`running on server: ${port}`);
+}
+
+// Initialize all route with a callback function
+app.get('/all', getData);
+app.post('/add', postData);
+
+// Callback function to complete GET '/all'
+function getData(req, res){
+    res.json(projectData);
+}
+
+// Post Route
+function postData(req, res) {
+    let newEntery = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        user_response: req.body.user_response,
+    }
+    projectData.push(newEntery);
+    res.json(projectData);
+}
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,79 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const baseUrl = 'https://community-open-weather-map.p.rapidapi.com/weather?zip=';
+const apiKey = '0e5a48fea2mshf0778f0385021bcp129542jsn6bb6cbfbc305';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+/* Async Function to GET Web API data */
+const getWeather = async (zip) => {
+
+  const response = await fetch(baseUrl + zip, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'x-rapidapi-key': apiKey
+    },
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+/* Async Function to POST data */
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      return newData;
+    } catch(error) {
+      console.log('error', error);
+    }
+};
+
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
+
+/* Function called by event listener */
+function performAction(e) {
+  const code = document.getElementById('zip').value;
+  getWeather(code)
+  .then(weather => {
+    postData('/add', {
+      temperature: weather.main.temp,
+      date: newDate,
+      user_response: document.getElementById('feelings').value
+    })
+    updateUI();
+  });
+}
+
+/* UPDATE UI DEMO */
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try {
+    const allData = await request.json();
+    console.log(allData);
+    document.getElementById('date').innerHTML = `Date: ${allData.slice(-1)[0].date}`;
+    document.getElementById('temp').innerHTML = `Temperature: ${allData.slice(-1)[0].temperature}`;
+    document.getElementById('content').innerHTML = `Feeling: ${allData.slice(-1)[0].user_response}`;
+  }
+  catch(error){
+    console.log('error ', error);
+  }
+}
